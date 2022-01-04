a193c976dc95509bd78224af662f278e69aaeefe
Vansh Gulati <vanshgulati16@gmail.com>
2022-01-05 02:16:08 +0530

    day 3 completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+// import 'package:matcher/matcher.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text("Welcome to flutter"),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          "Login Page",
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+          textScaleFactor: 1.0,
+        ),
+      ),
+    );
+  }
+}
